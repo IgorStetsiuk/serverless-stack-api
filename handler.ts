43ed58cf9187e13a3d329f3fc513688f15c5a319
Igor Stetsiuk <igorstetsuk@gmail.com>
2021-02-09 15:40:52 +0200

    Set up correct es-lint

--- handler.ts
+++ handler.ts
@@ -1,15 +1,21 @@
-import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+// eslint-disable-next-line import/no-unresolved
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
+// eslint-disable-next-line import/prefer-default-export
 export async function hello(
   event: APIGatewayEvent,
-  context: Context
+  context: Context,
 ): Promise<APIGatewayProxyResult> {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Go Serverless v2.0! Your function executed successfully!",
-      context,
+      message: `Go Serverless v2.0! ${(await message({ time: 1, copy: 'Your function executed successfully!' }))}`,
       event,
+      context,
     }),
   };
 }
+
+const message = ({ time, ...rest }) => new Promise((resolve) => setTimeout(() => {
+  resolve(`${rest.copy} (with a delay)`);
+}, time * 1000));
--- tests/handler.test.ts
+++ tests/handler.test.ts
@@ -1,12 +1,15 @@
-import {APIGatewayEvent, Context} from "aws-lambda";
-import * as handler from "../handler";
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import * as handler from '../handler';
 
-test("hello", async () => {
-  const event = { body: "Test Body" } as APIGatewayEvent;
+// eslint-disable-next-line no-undef
+test('hello', async () => {
+  const event = { body: 'Test Body' } as APIGatewayEvent;
   const context = {} as Context;
 
   const response = await handler.hello(event, context);
 
+  // eslint-disable-next-line no-undef
   expect(response.statusCode).toEqual(200);
-  expect(typeof response.body).toBe("string");
+  // eslint-disable-next-line no-undef
+  expect(typeof response.body).toBe('string');
 });
