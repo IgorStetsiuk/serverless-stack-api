c32009fef503b88b23a011470aa8df55b21c21ab
Igor Stetsiuk <igorstetsuk@gmail.com>
2021-02-11 17:37:38 +0200

    Created DynamoDB table, created create api, refactored code.

---
+++ libs/handler-lib.ts
@@ -0,0 +1,21 @@
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body;
+    let statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+    };
+  };
+}
